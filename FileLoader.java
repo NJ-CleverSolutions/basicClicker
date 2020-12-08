fdfc989386770868716bcbf7c60a32330cbb5a34
Nathaniel Johnson <70444394+GeneralCypher@users.noreply.github.com>
2020-12-08 17:13:05 -0500

    fileWriter complete fileLoader still needs decryption values

--- FileLoader.java
+++ FileLoader.java
@@ -1,10 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class FileLoader {
+
+    public ArrayList<String> info = new ArrayList<>();
 
     public FileLoader() {
 
     }
 
     public void reading() {
-        return;
+
+        File file = new File("userData.txt");
+
+        try {
+
+            FileReader fr = new FileReader(file);
+            BufferedReader bfr = new BufferedReader(fr);
+
+            String line = bfr.readLine();
+
+            while (line != null) {
+
+                info.add(line);
+                line = bfr.readLine();
+            }
+
+            bfr.close();
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public ArrayList<String> getInfo() {
+        return info;
     }
 }
--- FileWriter.java
+++ FileWriter.java
@@ -1,12 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class FileWriter {
 
-    public FileWriter()
-    {
+    public FileWriter() {
 
     }
 
-    public void save()
-    {
+    public void save() {
+        File file = new File("userData.txt");
 
+        try {
+
+            FileOutputStream fos = new FileOutputStream(file, true);
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+        PrintWriter pw = null;
+
+        try {
+
+            pw = new PrintWriter(file);
+
+            FileLoader fl = new FileLoader();
+
+            for (String line : fl.getInfo()) {
+                System.out.println(line);
+            }
+
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
     }
 }
